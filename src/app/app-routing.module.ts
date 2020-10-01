d42844bd7e12624ea939985a8db3e2cdf4bfa824
Sauvik Dolui <sauvikdolui.apple@gmail.com>
2020-10-01 20:18:44 +0530

    Add auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TalkToDoctorComponent } from './diagonisis/talk-to-doctor/talk-to-doctor.component';
 import { HealthDashboardComponent } from './health-dashboard/health-dashboard.component';
+import { AuthGuard } from './user/auth.guard';
 // import { HealthCheckUpComponent } from './health-check-up/health-check-up.component';
 // import { BuyMedicineComponent } from './buy-medicine/buy-medicine.component';
 // import { BookdrappointmentComponent } from './bookdrappointment/bookdrappointment.component';
@@ -12,22 +13,20 @@ const routes: Routes = [
     component: HealthDashboardComponent
   },
   {
-    path: 'talktodoc',
-    loadChildren: () =>
-      import('./diagonisis/diagonisis.module').then(m => m.DiagonisisModule)
-  },
-  {
     path: 'healthcheckup',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./check-up/check-up.module').then(m => m.CheckUpModule)
   },
   {
     path: 'buymedicine',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./marketing/marketing.module').then(m => m.MarketingModule)
   },
   {
     path: 'bookdrappointment',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./appointment/appointment.module').then(m => m.AppointmentModule)
   }
